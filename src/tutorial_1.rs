8ca337ea9ca987cf1bc67e74f36749c74d5b4605
Gian Alfonso <93459007+Imanoodle13@users.noreply.github.com>
2025-06-17 19:18:49 -0500

    Rust compilation with cargo

--- src/tutorial_1.rs
+++ src/tutorial_1.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+/*
+COMPILE AND RUN YOUR RUST CODE:
+1. Set up the following in Cargo.tml:
+    [[bin]]
+    name = "{program_name}"
+    path = "src/{program_name}.rs"
+2. `cargo build`:
+    Compiling rustTutorial v0.1.0 (C:\{directories}\{program_name})
+    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.24s 
+3. `cd target`
+4. `cd debug`
+5. ./{program_name}
+*/
