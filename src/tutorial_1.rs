19d70375888895878bbb2222bb712d4eeb23e164
Gian Alfonso <93459007+Imanoodle13@users.noreply.github.com>
2025-06-17 19:34:26 -0500

    cargo run command instruction refactor

--- src/tutorial_1.rs
+++ src/tutorial_1.rs
@@ -1,5 +1,25 @@
 fn main() {
     println!("Hello, world!");
+    println!();
+    println!("COMPILE AND RUN YOUR RUST CODE:
+1. Set up the following in Cargo.toml:
+    [[bin]]
+    name = \\\"{{program_name}}\"
+    path = \\\"src/{{program_name}}.rs\"
+2. `cd target`
+3. `cd debug`
+4. `cargo run`:
+    PS C:\\{{directories}}\\target\\debug> cargo run
+    Compiling rustTutorial v0.1.0 (C:\\{{directories}})
+    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.21s
+    Running `.\\{{program_name}}.exe`
+    Hello, world!
+
+Additionally:
+`cargo check`:
+-   It checks if the code can compile without actually compiling it.
+-   Not applicable at this scale but if you had a project that
+    utilizes Cargo, the outputs/errors will be listed.")
 }
 
 /*
@@ -8,10 +28,18 @@ COMPILE AND RUN YOUR RUST CODE:
     [[bin]]
     name = "{program_name}"
     path = "src/{program_name}.rs"
-2. `cargo build`:
-    Compiling rustTutorial v0.1.0 (C:\{directories}\{program_name})
-    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.24s 
-3. `cd target`
-4. `cd debug`
-5. ./{program_name}
+2. `cd target`
+3. `cd debug`
+4. `cargo run`:
+    PS C:\{directories}\target\debug> cargo run
+    Compiling rustTutorial v0.1.0 (C:\{directories})
+    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.21s
+    Running `.\{program_name}.exe`
+    Hello, world!
+
+Additionally:
+`cargo check`:
+-   It checks if the code can compile without actually compiling it.
+-   Not applicable at this scale but if you had a project that
+    utilizes Cargo, the outputs/errors will be listed.
 */
