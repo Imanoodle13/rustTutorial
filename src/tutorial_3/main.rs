50cc135c3d67d40e466991a2726a060fed959808
Gian Alfonso <93459007+Imanoodle13@users.noreply.github.com>
2025-06-28 16:48:05 -0500

    Add .gitignore files for tutorial directories and update variable mutability in main.rs

--- src/tutorial_3/main.rs
+++ src/tutorial_3/main.rs
@@ -1,4 +1,6 @@
 fn main(){
-    let x = 4; // Implicit type assignment. Assigned as an integer at compile time.
+    let mut x = 4; // Implicit type assignment. Assigned as an integer at compile time.
+    println!("x is : {}",x);
+    x = 5;
     println!("x is : {}",x);
 }
