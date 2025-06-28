4aa0be405c7a1648aa14955f73cd71c0fa17f12e
Gian Alfonso <93459007+Imanoodle13@users.noreply.github.com>
2025-06-28 17:04:52 -0500

    Lesson on mutable, immutable, and constants.

--- src/tutorial_3/main.rs
+++ src/tutorial_3/main.rs
@@ -2,4 +2,23 @@
 fn main(){
     let mut x = 4; // Implicit type assignment. Assigned as an integer at compile time.
     println!("x is : {}",x);
+
+    {
+        let x = 2;
+        println!("x is : {}",x);
+    }
+
+    x += 2;
+    println!("x is : {}",x);
+    println!();
+
+    // Complete reassignment of datatype and value is possible
+    let y = 4;
+    println!("y is : {}",y);
+    let y = "Hello!";
+    println!("y is : {}",y);
+    println!();
+
+    const MULTIPLIER: u32 = 7; // const is a stricter version of immutable variables.
+    print!("Multiplier is : {}",MULTIPLIER);
 }
